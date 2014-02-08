d4f5f74b850f1818e54d817e4c43ca1f73cb0a31
Evan Dale Aromin <avendael@gmail.com>
2014-02-08 14:19:04 +0800

    Simple playlist crud. Save peer_id to the user firebase.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,15 +14,33 @@ angular.module('jukedogeApp')
 
       // Wrap everything in checkLogin because the user must be logged in.
       loginService.checkLogin(function success(user) {
-        // Wait for peerjs connection before defining other functions
+        $log.info('login success');
+        $scope.userFirebase = $firebase(new Firebase(firebaseUrl + user.uid));
+        $scope.playlistFirebase = $firebase(new Firebase(firebaseUrl + user.uid + playlistUrl));
+
         peer.on('open', function(peerId) {
           $log.info('your peer id is ' + peerId);
-          $scope.userFirebase = $firebase(new Firebase(firebaseUrl + user.uid));
           $scope.userFirebase.peer_id = peerId;
           $scope.userFirebase.username = user.username;
 
           $scope.userFirebase.$save('peer_id');
           $scope.userFirebase.$save('username');
         });
+
+        $scope.addSong = function() {
+          $scope.playlistFirebase.$add({
+            title: 'song ' + Math.floor(Math.random() * 100)
+          });
+        };
+
+        $scope.updateSong = function(song) {
+          song.title = 'favorite song ' + Math.floor(Math.random() * 100);
+
+          $scope.playlistFirebase.$save();
+        };
+
+        $scope.removeSong = function(index) {
+          $scope.playlistFirebase.$remove($scope.playlistFirebase.$getIndex()[index]);
+        };
       });
     });
