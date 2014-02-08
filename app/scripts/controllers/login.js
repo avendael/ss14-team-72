9841c5706be92232418586175ae0d383eccf9bfe
Evan Dale Aromin <avendael@gmail.com>
2014-02-08 13:26:32 +0800

    Cleaned up login controller to use defined default callbacks in loginService.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,19 +4,7 @@ angular.module('jukedogeApp')
   .controller(
     'LoginCtrl',
     function ($scope, $location, $firebaseSimpleLogin, $log, firebaseUrl, loginService) {
-      // $scope.loginService = $firebaseSimpleLogin(new Firebase(firebaseUrl));
-
       $scope.onLoginGithub = function() {
-        loginService.loginGithub(function success(user) {
-          $location.path('/');
-        }, function fail(error) {
-          alert('login failed ' + JSON.stringify(error));
-        });
-        // $scope.loginService.$login('github').then(function success(user) {
-        //   $log.info('github login successful');
-        //   $location.path('/');
-        // }, function fail(error) {
-        //   alert('login failed  ' + JSON.stringify(error));
-        // });
+        loginService.loginGithub();
       };
   });
